0613adf9a409b4dd31559d03466dfa879193da75
Vittorio Ballestra <vittorio.ballestra@drafintech.it>
2017-07-17 09:17:02 +0200

    added @JS mapping

--- lib/vaadin_grid.dart
+++ lib/vaadin_grid.dart
@@ -1,5 +1,8 @@
+@JS('Vaadin')
+library vaadin;
 import 'package:polymer_element/polymer_element.dart';
 
+@JS('VaadinGrid')
 @PolymerRegister('vaadin-grid',native:true)
 @BowerImport(name:'vaadin-grid',ref:'vaadin/vaadin-grid#v3.0.0',import:'vaadin-grid/vaadin-grid.html')
 abstract class VaadinGrid extends PolymerElement {
@@ -7,6 +10,7 @@ abstract class VaadinGrid extends PolymerElement {
 }
 
 
+@JS('VaadinGridColumn')
 @PolymerRegister('vaadin-grid-column',native:true)
 @BowerImport(name:'vaadin-grid',ref:'vaadin/vaadin-grid#v3.0.0',import:'vaadin-grid/vaadin-grid-column.html')
 abstract class VaadinGridColumn extends PolymerElement {
